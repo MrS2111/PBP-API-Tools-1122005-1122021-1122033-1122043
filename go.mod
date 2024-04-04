dc0a9ddd7bfa51cdc36e685847f754e42ddd09fb
rafaeljoyhadi <youremail@example.com>
2024-04-04 11:58:33 +0700

    GoCron base

---
+++ go.mod
@@ -0,0 +1,13 @@
+module PBP-API-Tools-1122005-1122021-1122033-1122043
+
+go 1.21.6
+
+require (
+	github.com/go-co-op/gocron v1.37.0 // indirect
+	github.com/go-co-op/gocron/v2 v2.2.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20231219180239-dc181d75b848 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func main() {
+
+	s := gocron.NewScheduler(time.UTC)
+
+	// CONTOH BUAT DIIMPLEMENT NANTI
+	s.Every(1).Seconds().Do(func() {
+		fmt.Println("1")
+	})
+
+	s.Every(30).Seconds().Do(func() {
+		fmt.Println("30")
+	})
+
+	s.Every(1).Minute().Do(func() {
+		fmt.Println("1 Minute")
+	})
+
+	// CONTOH KALO PAKE TIMER
+	s.Every(1).Day().At("09:00").Do(func() {
+		fmt.Println("Good morning! This task runs every day at 9:00 AM.")
+	})
+
+	s.StartBlocking()
+
+	select {}
+}
